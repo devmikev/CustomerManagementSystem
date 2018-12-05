1e5d512d9157578c2942ffc80b9d5a50629d5863
Mike V <dev.mikev@gmail.com>
2018-12-05 10:29:44 -0500

    create Customer class and Automated Tests

---
+++ ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            // Arrange
+            Customer customer = new Customer() { FirstName = "Bilbo", LastName = "Baggins" };
+
+            string expected = "Baggins, Bilbo";
+
+            // Act
+            string actual = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            Customer customer = new Customer() { LastName = "Baggins" };
+            string expected = "Baggins";
+
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            Customer customer = new Customer() { FirstName = "Bilbo" };
+            string expected = "Bilbo";
+
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
